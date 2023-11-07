0522c51f2226df7f5e34558599d17d67f359c637
Tylander732 <tyler.harding.sde@gmail.com>
2023-11-07 14:03:02 -0800

    committing changes

---
+++ nvim/after/plugin/refactoring.lua
@@ -0,0 +1,49 @@
+require('refactoring').setup({
+    prompt_func_return_type = {
+        go = false,
+        java = false,
+
+        cpp = false,
+        c = false,
+        h = false,
+        hpp = false,
+        cxx = false,
+    },
+    prompt_func_param_type = {
+        go = false,
+        java = false,
+
+        cpp = false,
+        c = false,
+        h = false,
+        hpp = false,
+        cxx = false,
+    },
+    printf_statements = {},
+    print_var_statements = {},
+})
+
+-- load refactoring Telescope extension
+require("telescope").load_extension("refactoring")
+
+vim.keymap.set(
+	{"n", "x"},
+	"<leader>rr",
+	function() require('telescope').extensions.refactoring.refactors() end
+)
+
+-- You can also use below = true here to to change the position of the printf
+-- statement (or set two remaps for either one). This remap must be made in normal mode.
+vim.keymap.set(
+	"n",
+	"<leader>rp",
+	function() require('refactoring').debug.printf({below = false}) end
+)
+
+-- Print var
+
+vim.keymap.set({"x", "n"}, "<leader>rv", function() require('refactoring').debug.print_var() end)
+-- Supports both visual and normal mode
+
+vim.keymap.set("n", "<leader>rc", function() require('refactoring').debug.cleanup({}) end)
+-- Supports only normal mode
